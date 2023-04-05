19db135ae478c8e1646fd8d42f1bf38a351973d4
iliapst <iliapst@gmail.com>
2023-04-05 17:07:53 +0300

    assignment1

---
+++ src/main/java/org/example/sec01/assignment/FileService.java
@@ -0,0 +1,13 @@
+package org.example.sec01.assignment;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileService {
+
+    public static final Path PATH = Paths.get("src/main/resources/assignment/sec01");
+
+    private static String readFile(String fileName) {
+        return null;
+    }
+}
